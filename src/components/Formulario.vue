6276605df39fdcf50c49f95a6e874d2f53f3d415
bruna <bruna@teste.com>
2025-01-07 17:12:26 -0300

    aplicando o desafio na lista de tarefas

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,23 +2,23 @@
     const props = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocarFiltro', 'editaTarefaTemp']);
 </script>
 <template>
-    <form @submit.prevent="props.cadastraTarefa">
-        <div class="row">
-        <div class="col">
-            <input :value="props.tarefaTemp" @change="props.editaTarefaTemp" required type="text" placeholder="Digite a descrição da tarefa" class="form-control">
-        </div>
-        <div class="col-md-2">
-            <button class="btn btn-primary" type="submit">
-            Cadastrar
-            </button>
-        </div>
-        <div class="col-md-2">
-            <select @change="props.trocarFiltro" class="form-control">
-            <option value="todas">Todas as tarefas</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="finalizadas">Finalizadas</option>
-            </select>
-        </div>
-        </div>
-  </form>
+        <form @submit.prevent="props.cadastraTarefa">
+            <div class="row">
+                <div class="col">
+                    <input :value="props.tarefaTemp" @change="props.editaTarefaTemp" required type="text" placeholder="Digite a descrição da tarefa" class="form-control">
+                </div>
+                <div class="col-md-2">
+                    <button class="btn btn-primary" type="submit">
+                    Cadastrar
+                    </button>
+                </div>
+                <div class="col-md-2">
+                    <select @change="props.trocarFiltro" class="form-control">
+                        <option value="todas">Todas as tarefas</option>
+                        <option value="pendentes">Pendentes</option>
+                        <option value="finalizadas">Finalizadas</option>
+                    </select>
+                </div>
+            </div>
+    </form>
 </template>
--- src/components/ListaDeTarefas.vue
+++ src/components/ListaDeTarefas.vue
@@ -2,12 +2,13 @@
     const props = defineProps(['tarefas'])
 </script>
 <template>
-        <ul class="list list-group mt-4">
-        <li class="list-group-item" v-for="tarefa in props.tarefas">
-        <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" type="checkbox" :id="tarefa.titulo">
-        <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">{{ tarefa.titulo }}</label>
-        </li>
-    </ul>
+        <ul class="list list-group mt-4" v-if="props.tarefas.length > 0">
+            <li class="list-group-item" v-for="tarefa in props.tarefas">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" type="checkbox" :id="tarefa.titulo">
+            <label :class="{ done: tarefa.finalizada === true }" class="ms-3" :for="tarefa.titulo">{{ tarefa.titulo }}</label>
+            </li>
+        </ul>
+        <p class="text-muted" v-else>Não há tarefas pendentes!</p>
 </template>
 
 <style scoped>
